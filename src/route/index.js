58b1c0b5926a9d2beb8387235703ed94f7f61fe1
dmitro543 <123867495+dmitro543@users.noreply.github.com>
2023-10-10 14:06:14 +0300

    Update index.js

--- src/route/index.js
+++ src/route/index.js
@@ -1,119 +1,45 @@
-// Підключаємо технологію express для back-end сервера
-const express = require('express')
-// Cтворюємо роутер - місце, куди ми підключаємо ендпоїнти
-const router = express.Router()
+<!DOCTYPE html>
+<html>
+    <head>
+        <div>
+            {{#with js}}
+            <div>
+                <h1>{{name}}</h1>
 
-// ================================================================
+                {{#with data}}
+                <div>
+                   <p>name:{{name}}</p>
+                </div>
+                {{/with}} 
+                    <br>
 
-// router.get Створює нам один ентпоїнт
+                {{#each data}}
+                   <div>
+                     <p>description:{{description}}</p>
+                     <h2> history:{{history}}</h2>
+                     <h3> year: {{year}}</h3>
+                     <h4> founder: {{founder}}</h4>
+                   </div>
+                {{/each}}
+                    <br>
 
-//           ↙ тут вводимо шлях (PATH) до сторінки
-router.get('/', function (req, res) {
-  // res.render генерує нам HTML сторінку
+                 {{#each types}}
+                  <h5>
+                    types:{{types}}
+                  </h5>
+                 {{/each}}
+                    <br>
 
-  //            ↙ cюди вводимо назву файлу з сontainer
-  res.render('index', {})
-  //                  ↑↑ сюди вводимо JSON дані
-})
+                 {{#each santax}}
+                      <h6> variables:{{variables}} </h6>
+                      <p>  functions: {{functions}}</p>
+                      <p>  conditionals: {{conditionals}}</p>
+                     <span>loops: {{loops}}</span>
+                        <b>classes: {{classes}} </b>
+                 {{/each}}
 
-// ================================================================
-
-//              ↙ тут вводимо шлях (PATH) до сторінки
-router.get('/bio', function (req, res) {
-  //             ↙ cюди вводимо назву файлу з сontainer
-  res.render('bio', {
-    name: 'Albert Einstein',
-    birthdate: 'March 14, 1879',
-    birthplace:
-      'Ulm, Kingdom of Württemberg, German Empire',
-    deathdate: 'April 18, 1955',
-    deathplace: 'Princeton, New Jersey, United States',
-    nationality: 'Swiss, German, American',
-    occupation: 'Theoretical Physicist',
-    known_for: [
-      'Theory of Relativity',
-      'E=mc²',
-      'Photoelectric Effect',
-      'Brownian Motion',
-    ],
-    education: [
-      {
-        degree: 'Doctor of Philosophy',
-        field: 'Physics',
-        institution: 'University of Zurich',
-        year: 1905,
-      },
-    ],
-    notable_publications: [
-      {
-        title: 'On the Electrodynamics of Moving Bodies',
-        year: 1905,
-        publisher: 'Annalen der Physik',
-      },
-      {
-        title:
-          'Does the Inertia of a Body Depend Upon Its Energy Content?',
-        year: 1905,
-        publisher: 'Annalen der Physik',
-      },
-    ],
-    partners: [
-      {
-        name: 'Mileva Marić',
-        relationship: 'First wife',
-        years: '1903-1919',
-      },
-      {
-        name: 'Elsa Einstein',
-        relationship:
-          "Second wife, also Einstein's first cousin",
-        years: '1919-1936',
-      },
-    ],
-    awards: [
-      {
-        title: 'Nobel Prize in Physics',
-        year: 1921,
-        description:
-          'Awarded for his explanation of the photoelectric effect',
-      },
-    ],
-    influences: [
-      'Isaac Newton',
-      'James Clerk Maxwell',
-      'Hermann Minkowski',
-    ],
-    influenced: [
-      'Niels Bohr',
-      'Erwin Schrödinger',
-      'Werner Heisenberg',
-      'Richard Feynman',
-    ],
-    quotes: [
-      'Imagination is more important than knowledge.',
-      'I have no special talent. I am only passionately curious.',
-      'The important thing is not to stop questioning.',
-      'The most beautiful thing we can experience is the mysterious. It is the source of all true art and all science.',
-    ],
-    major_discoveries: [
-      {
-        title: 'Photoelectric Effect',
-        year: 1905,
-        description:
-          'Einstein explained the photoelectric effect, where electrons are emitted from a metal surface when it is illuminated by light.',
-      },
-    ],
-    contributions: {
-      title: 'Inventions',
-      description:
-        'Leonardo designed and invented a wide variety of machines and devices, including a helicopter, a diving suit, and a self-propelled cart. Many of his inventions were centuries ahead of their time.',
-      year: 'Late 15th to early 16th century',
-      field: 'Invention',
-    },
-  })
-})
-
-// ================================================================
-
-// Підключаємо роутер до бек-енду
-module.exports = router
+            </div>
+            {{/with}}
+        </div>
+    </head>
+</html>
